c9d981ea61ad302809a787c1151f56026b9af8fd
Klein254 <rayobiero62@gmail.com>
2023-04-12 12:47:48 +0300

    webite view

--- app/src/main/java/com/example/intents_android_obiero/Website_Activity.kt
+++ app/src/main/java/com/example/intents_android_obiero/Website_Activity.kt
@@ -2,10 +2,20 @@ package com.example.intents_android_obiero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class Website_Activity : AppCompatActivity() {
+
+    lateinit var myWeb:WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_website)
+
+        myWeb = findViewById(R.id.m_web)
+
+        val webSettings = myWeb.settings
+        webSettings.javaScriptEnabled = true
+        myWeb.loadUrl("https://klein254.github.io/Shop/")
+
     }
 }
